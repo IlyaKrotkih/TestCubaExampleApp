3f59fc68964d37e0d95e238a6467f0642828da30
Илья Кротких <ivashca99@gmail.com>
2017-01-29 20:49:15 +0300

    Add screens for "Nomenclature"

---
+++ modules/gui/src/com/company/testproject/gui/nomenclature/browse/NomenclatureBrowser.java
@@ -0,0 +1,22 @@
+package com.company.testproject.gui.nomenclature.browse;
+
+import java.util.Map;
+
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.*;
+
+import javax.inject.Inject;
+
+/**
+ * Created by ila on 27.01.2017.
+ */
+public class NomenclatureBrowser extends AbstractLookup {
+
+    @Inject
+    private Table table;
+
+    @Override
+    public void init(Map<String, Object> params) {
+
+    }
+}
