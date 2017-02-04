db2fc655e92721f83d3dde68d729076266c05643
Илья Кротких <ivashca99@gmail.com>
2017-02-04 19:22:03 +0300

    Add new screen: Nomenclature editor.

---
+++ modules/gui/src/com/company/testproject/gui/nomenclature/edit/NomenclatureEditor.java
@@ -0,0 +1,17 @@
+package com.company.testproject.gui.nomenclature.edit;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.company.testproject.entity.Nomenclature;
+
+import java.util.Map;
+
+/**
+ * Created by ila on 04.02.2017.
+ */
+public class NomenclatureEditor extends AbstractEditor<Nomenclature> {
+
+    @Override
+    public void init(Map<String, Object> params) {
+        //
+    }
+}
